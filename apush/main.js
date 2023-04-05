e5a35e137af86ce0ef6b61b85050f617d5e4947f
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-04-05 16:37:39 -0400

    1

---
+++ apush/main.js
@@ -0,0 +1,34 @@
+import "./style.css";
+
+let box = [];
+let position = "";
+
+let sprite =
+  "https://i.pinimg.com/originals/63/91/03/639103eff63de2e26c5e1ecd0365712f.png";
+for (let i = 1; i < 101; i++) {
+  box[i] = document.getElementById(`box${i}`);
+}
+box[1].innerHTML =
+  box[1].innerHTML = `<div><img class="sprite" src ="${sprite}"></div>`;
+
+function clear() {
+  box.forEach((box) => {
+    box.innerHTML = "";
+  });
+}
+
+box.forEach((box) => {
+  box.addEventListener("click", function () {
+    clear();
+    box.innerHTML = `<div><img class="sprite" src ="${sprite}"></div>`;
+    position = box.id;
+    console.log(position);
+    alertladder();
+  });
+});
+
+function alertladder() {
+  if (position === "box2") {
+    console.log("u reached a ladder");
+  }
+}
